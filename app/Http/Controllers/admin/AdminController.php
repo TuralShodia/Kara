79027e5676736b4c9d4ed84ecce023f1e15d818d
ElmarAliyev-7 <liyevelmar02@gmail.com>
2023-06-14 15:46:37 +0400

    fixed

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -8,49 +8,75 @@ use App\Models\Order;
 use App\Models\Category;
 use App\Http\Requests\OrderRequest;
 use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $book=Book::count();
         $category=Category::count();
         $user=User::count();
         return view('admin/dashboard',compact('book','category','user'));
     }
-    public function orderTable(){
-        // return User::where('id', 10)->with('books')->first();
-        // return Book::where('id', 1)->with('users')->first();
 
+    public function orderTable()
+    {
         $orders=Order::all();
-        
+
         foreach($orders as $key=>$order){
             $orders[$key]['book_name'] = $this->getBookNameById($order->book_id);
             $orders[$key]['user_name'] = $this->getUserNameById($order->user_id);
         }
         return view('admin/order',compact('orders'));
     }
-    public function getBookImageById($book_id){
+
+    public function dowloadOrdersPdf()
+    {
+        $orders=Order::all();
+
+        $data = $orders->map(function ($order) {
+            $order['book_image'] = $this->getBookImageById($order->book_id);
+            $order['book_name'] = $this->getBookNameById($order->book_id);
+            $order['user_name'] = $this->getUserNameById($order->user_id);
+            return $order;
+        });
+
+        $orders = $data->toArray();
+        return view('front.pdf.orders', compact('orders'));
+    }
+
+
+
+    public function getBookImageById($book_id)
+    {
         if(!Book::find($book_id)){
             return 'Book Deleted';
         }
         return Book::find($book_id)->image;
     }
-    public function getBookNameById($book_id){
+
+    public function getBookNameById($book_id)
+    {
         if(!Book::find($book_id)){
             return 'Book Deleted';
         }
         return Book::find($book_id)->name;
     }
-    public function getUserNameById($user_id){
+
+    public function getUserNameById($user_id)
+    {
         if(!User::find($user_id)){
             return 'User Deleted';
         }
         return User::find($user_id)->name;
     }
 
-    public function order($id){
+    public function order($id)
+    {
         if(Auth::user() and Auth::user()->role_id==1)
         {
             $characters = '123456ABCDEF';
@@ -60,19 +86,22 @@ class AdminController extends Controller
                 'user_id'=>Auth::id(),
                 'promocode'=>substr(str_shuffle($characters), 0, 6)
             ]);
-            return redirect()->back()->with('success','Book Ordered Successfully ');  
+            return redirect()->back()->with('success','Book Ordered Successfully ');
         }else{
-            return redirect()->route('user.login');  
+            return redirect()->route('user.login');
         }
-        
+
     }
+
     public function delete($id)
     {
         $order=Order::findOrFail($id);
         $order->delete();
         return redirect()->route('order')->with('success','Order Deleted Successfully');;
     }
-    public function orders($id){
+
+    public function orders($id)
+    {
         $orders=Order::where('user_id',$id)->get();
         foreach($orders as $key=>$order){
             $orders[$key]['book_name'] = $this->getBookNameById($order->book_id);
@@ -80,10 +109,10 @@ class AdminController extends Controller
         }
         return view('front.orders',compact('orders'));
     }
+
     public function orderCancel($id){
         $order=Order::findOrFail($id);
         $order->delete();
         return redirect()->back()->with('success','Order Canceled Successfully');;
     }
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -40,13 +40,13 @@ Route::group(['prefix'=>'/admin'], function(){
 
           Route::get('/profile/edit',[ProfileController::class, 'index'])->name('profile');
           Route::post('/profile/update',[ProfileController::class, 'update'])->name('profile.update');
-  
-  
+
+
           Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
-  
+
           Route::get('/message',[MessageController::class,'index'])->name('message');
           Route::get('/message/delete/{id}',[MessageController::class, 'delete'])->name('message.delete');
-          
+
           Route::get('/orders',[AdminController::class, 'orderTable'])->name('order');
           Route::get('/orders/delete/{id}',[AdminController::class, 'delete'])->name('order.delete');
 
@@ -55,17 +55,17 @@ Route::group(['prefix'=>'/admin'], function(){
           Route::get('/info/edit/{id}',[NewsController::class, 'edit'])->name('info.edit');
           Route::post('/info/update/{id}',[NewsController::class, 'update'])->name('info.update');
           Route::get('/info/delete/{id}',[NewsController::class, 'delete'])->name('info.delete');
-  
+
           Route::get('/contacts/edit',[ContactController::class, 'index'])->name('contacts');
           Route::post('/contacts/update',[ContactController::class, 'update'])->name('contacts.update');
-  
+
           Route::get('/categories',[CategoriesController::class, 'index'])->name('categories');
           Route::post('/categories',[CategoriesController::class, 'store'])->name('categories.submit');
           Route::get('/categories/edit/{id}',[CategoriesController::class, 'edit'])->name('categories.edit');
           Route::post('/categories/update/{id}',[CategoriesController::class, 'update'])->name('categories.update');
           Route::get('/categories/delete/{id}',[CategoriesController::class, 'delete'])->name('categories.delete');
-  
-  
+
+
           Route::get('/book',[BookController::class, 'index'])->name('book');
           Route::post('/book',[BookController::class, 'store'])->name('book.submit');
           Route::get('/book/edit/{id}',[BookController::class, 'edit'])->name('book.edit');
@@ -86,8 +86,8 @@ Route::group(['prefix'=>'/admin'], function(){
 
           Route::get('/abouts/edit',[AboutController::class, 'index'])->name('about');
           Route::post('/abouts/update',[AboutController::class, 'update'])->name('about.update');
-  
-          
+
+
 
           Route::get('/users',[UserController::class,'index'])->name('user');
           Route::post('/users',[UserController::class, 'store'])->name('user.submit');
@@ -97,33 +97,34 @@ Route::group(['prefix'=>'/admin'], function(){
 
         });
     });
-    Route::get('/search',[FrontController::class,'search'])->name('search');
 
-      Route::get('/',[FrontController::class,'index'])->name('home');
-      Route::get('/books',[FrontController::class,'books'])->name('front.book');
-      Route::get('/books-single/{book}',[FrontController::class,'booksSingle'])->name('book.single');
+Route::get('/search',[FrontController::class,'search'])->name('search');
 
-      Route::get('/books/{id}',[FrontController::class,'category'])->name('category');
+Route::get('/',[FrontController::class,'index'])->name('home');
+  Route::get('/books',[FrontController::class,'books'])->name('front.book');
+  Route::get('/books-single/{book}',[FrontController::class,'booksSingle'])->name('book.single');
 
-      Route::get('/news',[FrontController::class,'news'])->name('front.news');
-      Route::get('/news-single/{news}',[FrontController::class,'newsSingle'])->name('front.news.single');
-      Route::get('/about-us',[FrontController::class,'about'])->name('front.about');
-      Route::get('/message',[FrontController::class,'message'])->name('front.message');
-      Route::post('/message',[MessageController::class,'message'])->name('front.message.submit');
+  Route::get('/books/{id}',[FrontController::class,'category'])->name('category');
 
-      Route::get('/user/sign-up',[UserController::class,'register'])->name('user.register');
-      Route::post('/user/sign-up/submit',[UserController::class,'registerSubmit'])->name('user.register.submit');
+  Route::get('/news',[FrontController::class,'news'])->name('front.news');
+  Route::get('/news-single/{news}',[FrontController::class,'newsSingle'])->name('front.news.single');
+  Route::get('/about-us',[FrontController::class,'about'])->name('front.about');
+  Route::get('/message',[FrontController::class,'message'])->name('front.message');
+  Route::post('/message',[MessageController::class,'message'])->name('front.message.submit');
 
-      Route::get('/user/login',[UserController::class,'login'])->name('user.login');
-      Route::post('/user/login-submit',[UserController::class,'loginSubmit'])->name('user.login.submit');
-      Route::group(['middleware'=>'userLogin'] ,function(){
-        Route::get('/order/{id}',[AdminController::class, 'order'])->name('front.order');
-        Route::get('/order/table/{id}',[AdminController::class, 'orders'])->name('front.orders');
-        Route::get('/order/cancel/{id}',[AdminController::class, 'orderCancel'])->name('order.cancel');
-      });
-      
-      
+  Route::get('/user/sign-up',[UserController::class,'register'])->name('user.register');
+  Route::post('/user/sign-up/submit',[UserController::class,'registerSubmit'])->name('user.register.submit');
 
-      Route::get('/user/logout',[UserController::class, 'logout'])->name('user.logout');
-      // Route::get('/users',[FrontController::class,'edit'])->name('user');
+  Route::get('/user/login',[UserController::class,'login'])->name('user.login');
+  Route::post('/user/login-submit',[UserController::class,'loginSubmit'])->name('user.login.submit');
+
+  Route::group(['middleware'=>'userLogin'] ,function() {
+    Route::get('/order/{id}',[AdminController::class, 'order'])->name('front.order');
+    Route::get('/order/table/{id}',[AdminController::class, 'orders'])->name('front.orders');
+    Route::get('/orders/pdf',[AdminController::class, 'dowloadOrdersPdf'])->name('front.download-orders-pdf');
+    Route::get('/order/cancel/{id}',[AdminController::class, 'orderCancel'])->name('order.cancel');
+  });
+
+  Route::get('/user/logout',[UserController::class, 'logout'])->name('user.logout');
+  // Route::get('/users',[FrontController::class,'edit'])->name('user');
 
